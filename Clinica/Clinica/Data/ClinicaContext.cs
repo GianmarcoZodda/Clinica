c06004af2ccfc340a6e3f50752fe84acfd6238f4
gianmarco zodda <zoddagj@gmail.com>
2024-05-12 13:20:09 -0300

    creacion del contexto de base de datos

---
+++ Clinica/Clinica/Data/ClinicaContext.cs
@@ -0,0 +1,22 @@
+﻿using Clinica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinica.Data
+{
+    public class ClinicaContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+
+        public ClinicaContext(DbContextOptions<ClinicaContext> options) : base(options) { }
+
+        public DbSet<Consultorio> Consultorios { get; set; }
+        public DbSet<Diagnostico> Diagnosticos { get; set; }
+        public DbSet<Especialidad> Especialidades { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Turno> Turnos { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+    }
+}
